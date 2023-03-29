b3fc28b4eb99bd2655659c28cac9db996db5c1f6
MR. YILDIRIM <nyma9260@gmail.com>
2023-03-29 21:33:34 +0100

    connection to API

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,35 @@ import { useState } from "react";
 import WeatherForecast from "./WeatherForecast";
 export default function Home() {
   const [searchTerm, setSearchTerm] = useState("");
+  const [weatherInfo, setWeatherInfo] = useState(null);
+
+  const getWeatherInfo = async () => {
+    try {
+      let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchTerm}&appid=${process.env.REACT_APP_OPEN_WEATHER_KEY}`;
+
+      let res = await fetch(url);
+      let data = await res.json();
+
+      const { main, name, sys, weather, wind } = data;
+
+      const newWeatherInfo = {
+        humidity: main.humidity,
+        location: `${name}, ${sys.country}`,
+        presssure: main.pressure,
+        sunset: sys.sunset,
+        temperature: main.temp,
+        weatherIcon: weather[0].icon,
+        weatherType: weather[0].main,
+        windSpeed: wind.speed,
+      };
+
+      console.log(data);
+      setWeatherInfo(newWeatherInfo);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <>
       <form role="search">
@@ -12,9 +41,11 @@ export default function Home() {
           value={searchTerm}
           onChange={(event) => setSearchTerm(event.target.value)}
         />
-        <button type="button">Search</button>
+        <button type="button" onClick={() => getWeatherInfo()}>
+          Search
+        </button>
       </form>
-      <WeatherForecast />
+      {weatherInfo && <WeatherForecast />}
     </>
   );
 }
