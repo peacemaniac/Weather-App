21649aedc552d946b644b550fe16e158b2ff2e71
MR. YILDIRIM <nyma9260@gmail.com>
2023-03-29 20:51:27 +0100

    add styles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
 import { useState } from "react";
 import Home from "./components/Home";
-import WeatherForecast from "./components/WeatherForecast";
+
 import "./styles/styles.scss";
 
 function App() {
-  const [searchTerm, setSearchTerm] = useState("");
-  return (
-    <>
-      <form role="search">
-        <input
-          type="search"
-          aria-label="Search for a city"
-          placeholder="London"
-          value={searchTerm}
-          onChange={(event) => setSearchTerm(event.target.value)}
-        />
-        <button type="button">Search</button>
-      </form>
-      <WeatherForecast />
-    </>
-  );
+  return <Home />;
 }
 
 export default App;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,3 +1,20 @@
+import { useState } from "react";
+import WeatherForecast from "./WeatherForecast";
 export default function Home() {
-  return <section>Search</section>;
+  const [searchTerm, setSearchTerm] = useState("");
+  return (
+    <>
+      <form role="search">
+        <input
+          type="search"
+          aria-label="Search for a city"
+          placeholder="London"
+          value={searchTerm}
+          onChange={(event) => setSearchTerm(event.target.value)}
+        />
+        <button type="button">Search</button>
+      </form>
+      <WeatherForecast />
+    </>
+  );
 }
