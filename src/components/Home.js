0dd58667bfba10dd2f0217fa3c7ebd410c17389e
MR. YILDIRIM <nyma9260@gmail.com>
2023-04-03 19:14:37 +0100

    update date automatically

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,35 +1,39 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import WeatherForecast from "./WeatherForecast";
 export default function Home() {
   const [searchTerm, setSearchTerm] = useState("");
   const [weatherInfo, setWeatherInfo] = useState(null);
 
-  const getWeatherInfo = async () => {
-    try {
-      let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchTerm}&appid=${process.env.REACT_APP_OPEN_WEATHER_KEY}&units=metric`;
+  useEffect(() => {
+    const getWeatherInfo = async () => {
+      try {
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchTerm}&appid=${process.env.REACT_APP_OPEN_WEATHER_KEY}&units=metric`;
 
-      let res = await fetch(url);
-      let data = await res.json();
+        let res = await fetch(url);
+        let data = await res.json();
 
-      const { main, name, sys, weather, wind } = data;
+        const { main, name, sys, weather, wind } = data;
 
-      const newWeatherInfo = {
-        humidity: main.humidity,
-        location: `${name}, ${sys.country}`,
-        pressure: main.pressure,
-        sunset: sys.sunset,
-        temperature: main.temp,
-        weatherIcon: weather[0].icon,
-        weatherType: weather[0].main,
-        windSpeed: wind.speed,
-      };
+        const newWeatherInfo = {
+          humidity: main.humidity,
+          location: `${name}, ${sys.country}`,
+          pressure: main.pressure,
+          sunset: sys.sunset,
+          temperature: main.temp,
+          weatherIcon: weather[0].icon,
+          weatherType: weather[0].main,
+          windSpeed: wind.speed,
+        };
 
-      console.log(data);
-      setWeatherInfo(newWeatherInfo);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+        console.log(data);
+        setWeatherInfo(newWeatherInfo);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getWeatherInfo();
+  }, [searchTerm]);
 
   return (
     <>
@@ -41,9 +45,9 @@ export default function Home() {
           value={searchTerm}
           onChange={(event) => setSearchTerm(event.target.value)}
         />
-        <button type="button" onClick={() => getWeatherInfo()}>
+        {/* <button type="button" onClick={() => getWeatherInfo()}>
           Search
-        </button>
+        </button> */}
       </form>
       {weatherInfo && <WeatherForecast {...weatherInfo} />}
     </>
